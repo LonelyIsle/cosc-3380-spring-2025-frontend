5387393875f3ed50a57281d9b6cf69d5023f6aef
William <d.j.stewart7873@gmail.com>
2025-03-16 19:56:32 -0500

    Update the login to contain the logo and fix the spacing of the buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/bag-full-logo.svg"; // Adjust path based on the location of the logo
 
 function Login() {
   const navigate = useNavigate();
@@ -7,6 +8,9 @@ function Login() {
   return (
     <div className="center-container">
       <div className="p-6 bg-mantle text-text rounded-xl shadow-lg">
+        <div className="flex justify-center mb-4">
+          <img src={logo} alt="Logo" className="w-64 h-64" />
+        </div>
         <h1 className="text-lg font-bold mb-4 text-subtext1">Please sign in</h1>
         <form>
           <div className="mb-2">
@@ -31,21 +35,24 @@ function Login() {
               placeholder="Enter Password"
             />
           </div>
-          <div className="mt-4 flex space-x-4">
-            <button className="bg-maroon text-base py-2 px-4 rounded  hover:bg-peach">
+          <div className="mt-6 text-center">
+            <button className="bg-maroon text-base py-2 px-4 rounded hover:bg-peach">
               Sign In
-            </button>
-            <button 
-              className="bg-green text-base py-2 px-4 rounded hover:bg-teal"
-              onClick={(e) => {
-                e.preventDefault();
-                navigate("/register");
-              }}
-            >
-              Register
             </button>
           </div>
         </form>
+        <div className="mt-6 text-center">
+          <p className="text-subtext0">Don't have an account?</p>
+          <button
+            className="bg-green text-base py-2 px-4 rounded hover:bg-teal"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/register");
+            }}
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
