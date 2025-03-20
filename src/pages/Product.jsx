c4fea4e96efbcccb568d01fdb694b79c537d77c3
BubufO <yusuf@ozgel.com>
2025-03-19 23:45:14 -0500

    Add Product page with details update routing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import products from "../products.js";
+import products from "../Products.jsx";
 
 function Product() {
   const { id } = useParams();
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import products from "../products.js";
+import products from "../Products.jsx";
 
 function Shop() {
   const navigate = useNavigate();
