869391fe698d163c1f9c234be9f3e6770424d68c
sebvu <santosjester738@gmail.com>
2025-03-22 19:13:55 -0500

    Cleanup commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,3 @@
-export { default as AddToCartButton } from "./AddToCartButton";
 export { default as Footer } from "./Footer";
 export { default as LoginValidation } from "./LoginValidation";
 export { default as Navbar } from "./Navbar";
