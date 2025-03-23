9366049cb688abd62197e98545e592e26fb989a5
sebvu <santosjester738@gmail.com>
2025-03-23 17:32:17 -0500

    Rename CartContext to ShopContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext.jsx";
+import { ShopProvider } from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <CartProvider>
+      <ShopProvider>
         <App />
-      </CartProvider>
+      </ShopProvider>
     </BrowserRouter>
   </StrictMode>,
 );
